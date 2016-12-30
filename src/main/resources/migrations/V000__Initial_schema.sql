82811b58a2c26850537a456e6b921976fc62247e
Alen Hrga <ahrga93@gmail.com>
2016-12-30 14:07:30 +0100

    Finished initial schema and indexes of frms database

--- src/main/resources/migrations/V000__Initial_schema.sql
+++ src/main/resources/migrations/V000__Initial_schema.sql
@@ -13,15 +13,19 @@ CREATE TABLE users (
   password     VARCHAR(100)       NOT NULL,
   phone_number VARCHAR(20),
   role         VARCHAR(15)        NOT NULL,
-  CONSTRAINT pk_users PRIMARY KEY (email),
+  CONSTRAINT pk_users PRIMARY KEY (id),
   CONSTRAINT fk_user_role FOREIGN KEY (role) REFERENCES user_roles (role)
 );
+CREATE INDEX idx_users_email
+  ON users (email);
+CREATE INDEX idx_users_role
+  ON users (role);
 
 -- types of companies, can be hierarchic so they need to have base type (parent)
--- example: COMPUTING -> SOFTWARE_ENGINEERING, TELECOMMUNICATION, ELECTROTECHNIC -> AUTOMATION
+-- example: COMPUTING -> SOFTWARE_ENGINEERING, TELECOMMUNICATIONS, ELECTROTECHNIC -> AUTOMATION
 CREATE TABLE company_type (
-  id      INTEGER      NOT NULL,
-  name    VARCHAR(100) NOT NULL,
+  id      INTEGER             NOT NULL,
+  name    VARCHAR(100) UNIQUE NOT NULL,
   base_id INTEGER DEFAULT NULL,
   CONSTRAINT pk_company_type PRIMARY KEY (id),
   CONSTRAINT fk_base_company_type FOREIGN KEY (base_id) REFERENCES company_type (id)
@@ -36,10 +40,15 @@ CREATE TABLE company (
   web_address VARCHAR(100),
   address     VARCHAR(150),
   notes       TEXT,
-  type        INTEGER             NOT NULL,
+  type        VARCHAR(100)        NOT NULL,
   CONSTRAINT pk_company PRIMARY KEY (id),
-  CONSTRAINT fk_company_company_type FOREIGN KEY (type) REFERENCES company_type (id)
+  CONSTRAINT fk_company_company_type FOREIGN KEY (type) REFERENCES company_type (name)
 );
+
+CREATE INDEX idx_company_name
+  ON company (name);
+CREATE INDEX idx_company_short_name
+  ON company (short_name);
 
 -- contest, workshop or some other event that require calls for sponsors and sponsorship tasks management
 CREATE TABLE event (
@@ -49,6 +58,10 @@ CREATE TABLE event (
   year       CHAR(4)      NOT NULL,
   CONSTRAINT pk_event PRIMARY KEY (id)
 );
+CREATE INDEX idx_event_name
+  ON event (name);
+CREATE INDEX idx_event_short_name
+  ON event (short_name);
 
 -- lookup table: for now it will contain information about types of sponsorships: MATERIAL, FINANCIAL
 CREATE TABLE sponsorship_type (
@@ -68,7 +81,7 @@ CREATE TABLE task (
   id             SERIAL      NOT NULL,
   event_id       INTEGER     NOT NULL,
   company_id     INTEGER     NOT NULL,
-  assignee       INTEGER     DEFAULT NULL,
+  assignee_id    INTEGER     DEFAULT NULL,
   type           VARCHAR(20) NOT NULL,
   call_time      TIMESTAMP   DEFAULT NULL,
   mail_time      TIMESTAMP   DEFAULT NULL,
@@ -77,5 +90,9 @@ CREATE TABLE task (
   notes          TEXT        DEFAULT NULL,
   CONSTRAINT pk_task PRIMARY KEY (id),
   CONSTRAINT fk_task_event_id FOREIGN KEY (event_id) REFERENCES event (id),
-  CONSTRAINT fk_task_company_id FOREIGN KEY (company_id) REFERENCES company (id)
+  CONSTRAINT fk_task_company_id FOREIGN KEY (company_id) REFERENCES company (id),
+  CONSTRAINT fk_task_assignee FOREIGN KEY (assignee_id) REFERENCES users (id)
 );
+
+CREATE INDEX idx_call_mail_follow_up_time
+  ON task (call_time, mail_time, follow_up_time);
