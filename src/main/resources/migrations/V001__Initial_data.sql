eea8649d8124f36e9d3a8607541a7ca2b5687c92
Alen Hrga <ahrga93@gmail.com>
2017-01-16 17:27:56 +0100

    commented example user

--- src/main/resources/migrations/V001__Initial_data.sql
+++ src/main/resources/migrations/V001__Initial_data.sql
@@ -2,8 +2,9 @@ INSERT INTO user_roles(role) VALUES ('USER');
 INSERT INTO user_roles(role) VALUES ('COORDINATOR');
 INSERT INTO user_roles(role) VALUES ('ADMIN');
 
-INSERT INTO users(first_name, last_name, email, password, phone_number, role)
-VALUES ('John', 'Doe', 'john.doe@gmail.com', '$2a$10$b1H5t0FflsFMox0oqgcaQO/J5n9foo05lbA36h17fqj1dEx.yeyW2', '0911111111', 'USER');
+-- commented for now
+-- INSERT INTO users(first_name, last_name, email, password, phone_number, role)
+-- VALUES ('John', 'Doe', 'john.doe@gmail.com', '$2a$10$b1H5t0FflsFMox0oqgcaQO/J5n9foo05lbA36h17fqj1dEx.yeyW2', '0911111111', 'USER');
 
 INSERT INTO company_type(id, name, base_id) VALUES (1, 'COMPUTING', null);
 INSERT INTO company_type(id, name, base_id) VALUES (2, 'SOFTWARE_ENGINEERING', 1);
